3e7a274229b9171b282c868bd782090cb4cabea2
ericjcili <ericjcili@gmai.com>
2021-09-12 21:54:40 -0400

    added sql server

--- app.js
+++ app.js
@@ -3,8 +3,68 @@ const inquirer = require('inquirer');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    port: '3306',
     user: 'root',
-    password:'password',
-    database: 'employeesDB'
+    password : 'password',
+    database: 'employeesDB',
+    port: 3306
+  });
+//Open ports: 55300, 55617, 57621
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    console.log(`
+    ╔═══╗─────╔╗──────────────╔═╗╔═╗
+    ║╔══╝─────║║──────────────║║╚╝║║
+    ║╚══╦╗╔╦══╣║╔══╦╗─╔╦══╦══╗║╔╗╔╗╠══╦═╗╔══╦══╦══╦═╗
+    ║╔══╣╚╝║╔╗║║║╔╗║║─║║║═╣║═╣║║║║║║╔╗║╔╗╣╔╗║╔╗║║═╣╔╝
+    ║╚══╣║║║╚╝║╚╣╚╝║╚═╝║║═╣║═╣║║║║║║╔╗║║║║╔╗║╚╝║║═╣║
+    ╚═══╩╩╩╣╔═╩═╩══╩═╗╔╩══╩══╝╚╝╚╝╚╩╝╚╩╝╚╩╝╚╩═╗╠══╩╝
+    ───────║║──────╔═╝║─────────────────────╔═╝║
+    ───────╚╝──────╚══╝─────────────────────╚══╝`)
+    startApp();
 });
+
+function startApp() {
+    inquirer
+        .prompt({
+            type: "list",
+            name: "task",
+            message: "Please enter selection: ",
+            choices: [
+                "View Employees",
+                "View Employees by Department",
+                "Add Employee",
+                "Remove Employees",
+                "Update Employee Role",
+                "Add Role",
+                "Exit"
+            ]
+        })
+        .then(function ({task}) {
+            switch(task) {
+                case "View Employees":
+                    viewEmployees();
+                    break;
+                case "View Employees by Department":
+                    viewEmployeesbyDepartment();
+                    break;
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Remove Employees":
+                    removeEmployees();
+                    break;
+                case "Update Employee Role":
+                    updateEmployeeRole();
+                    break;
+                case "Add Role":
+                    addRole();
+                    break;
+                case "Exit":
+                    exit();
+                    connection.end();
+                    break;
+            }
+        });
+}
